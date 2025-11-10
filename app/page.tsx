c9a1f6cafc0ae090530d6d787ff2a816cc86e43e
haraphat01 <haraphat@gmail.com>
2025-11-10 06:58:47 -0800

    added youtube video

--- app/page.tsx
+++ app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
               <div className="relative w-full" style={{ paddingBottom: '56.25%' }}>
                 <iframe
                   className="absolute top-0 left-0 w-full h-full rounded-lg"
-                  src="https://www.youtube.com/embed/YOUR_VIDEO_ID"
+                  src="https://youtu.be/jfzbRRNN-lk"
                   title="Dealping Demo Video"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                   allowFullScreen
